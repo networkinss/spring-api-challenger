31f9b54242f549a9f6d0286716007bd81235fa2e
networkinss <oliver.glas@inss.ch>
2022-02-26 18:45:43 +0100

    initial commit troublemaker stub.

---
+++ src/main/java/ch/inss/springtroublemaker/controller/BooksApiController.java
@@ -0,0 +1,27 @@
+package ch.inss.springtroublemaker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-02-26T18:39:00.977440+01:00[Europe/Zurich]")
+@Controller
+@RequestMapping("${openapi.petstoretroublemaker.base-path:/api/v3}")
+public class BooksApiController implements BooksApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public BooksApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
