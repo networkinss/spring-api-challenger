7fbd70eb72665d744ba1867c43ae93542a3a76ae
networkinss <oliver.glas@inss.ch>
2022-03-12 15:06:33 +0100

    order/inventory with values in random order.

--- src/main/java/ch/inss/springtroublemaker/controller/StoreApiController.java
+++ src/main/java/ch/inss/springtroublemaker/controller/StoreApiController.java
@@ -1,10 +1,13 @@
 package ch.inss.springtroublemaker.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.request.NativeWebRequest;
-import java.util.Optional;
+
+import java.util.*;
 import javax.annotation.Generated;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-02-26T19:49:48.701044+01:00[Europe/Zurich]")
@@ -24,4 +27,16 @@ public class StoreApiController implements StoreApi {
         return Optional.ofNullable(request);
     }
 
+    @Override
+    public ResponseEntity<Map<String, Integer>> getInventory() {
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        int r = new Double(Math.random() * 3).intValue();
+        System.out.println("R = " + r);
+        for (int i=0;i<=3;i++){
+            if (r > 3) r = 0;
+            map.put("additionalProp" + r,r);
+            r++;
+        }
+        return new ResponseEntity<Map<String, Integer>>(map, HttpStatus.OK);
+    }
 }
